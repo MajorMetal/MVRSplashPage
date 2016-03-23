ed2e17b7869ae6c2f86a9702e72846af1d29cef1
MajorMetal <MajorMetal@clownfart>
2016-03-22 20:57:56 -0700

    ios test

--- public/application.js
+++ public/application.js
@@ -1,7 +1,7 @@
 function ready () {
-			var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+			// var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-			if (isSafari) {
+			if (iOSversion()) {
 				var container = document.querySelector('#iframe-container'),
 					iframe = document.createElement('iframe');
 
@@ -19,13 +19,17 @@ function ready () {
 
 function iOSversion() { 
 	if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) { 
-		if (!!window.indexedDB) { return 'iOS 8 and up'; } 
-		if (!!window.SpeechSynthesisUtterance) { return 'iOS 7'; } 
-		if (!!window.webkitAudioContext) { return 'iOS 6'; } 
-		if (!!window.matchMedia) { return 'iOS 5'; } 
-		if (!!window.history && 'pushState' in window.history) 
-			{ return 'iOS 4'; } 
-		return 'iOS 3 or earlier'; } 
-	return 'Not an iOS device'; }
+		// if (!!window.indexedDB) { return 'iOS 8 and up'; } 
+		// if (!!window.SpeechSynthesisUtterance) { return 'iOS 7'; } 
+		// if (!!window.webkitAudioContext) { return 'iOS 6'; } 
+		// if (!!window.matchMedia) { return 'iOS 5'; } 
+		// if (!!window.history && 'pushState' in window.history) 
+		// 	{ return 'iOS 4'; } 
+		// return 'iOS 3 or earlier';
+		return true;
+	} 
+	// return 'Not an iOS device';
+	return false;
+}
 
 document.addEventListener('DOMContentLoaded', ready);
