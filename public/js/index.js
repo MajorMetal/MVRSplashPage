e114d2e008aa35070f4370bc1ec78210e25ad1d3
MajorMetal <MajorMetal@clownfart>
2016-03-22 21:31:04 -0700

    ios test

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,17 @@
 function ready () {
 			// var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-			console.log('going here?');
 
 			if (iOSversion()) {
-				console.log('is iOS');
-				var container = document.querySelector('#iframe-container'),
-					iframe = document.createElement('iframe');
+				var iframe = document.querySelector('#video'),
+					// iframe = document.createElement('iframe');
 
-				iframe.width = '560';
-				iframe.height = '315';
-				iframe.src = 'https://www.youtube.com/embed/TuapDf6Un7M';
-				iframe.frameborder = '0';
-				iframe.allowfullscreen = true;
+				iframe.width 	= '560';
+				iframe.height 	= '315';
+				iframe.src 		= 'https://www.youtube.com/embed/TuapDf6Un7M';
+				// iframe.frameborder = '0';
+				// iframe.allowfullscreen = true;
 
-				container.appendChild(iframe);
-			} else {
-				console.log('is not iOS');
+				// container.appendChild(iframe);
 			}
 		}
 		
