e1226a03ffa2764f5427df4e1f0cf2689c68e731
MajorMetal <MajorMetal@clownfart>
2016-03-23 11:57:15 -0700

    ios test

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,18 @@
 function ready () {
-			// var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-			if (iOSversion()) {
-				var iframe = document.querySelector('#video');
-					// iframe = document.createElement('iframe');
+	if (iOSversion()) {
+		var iframe = document.querySelector('#video');
 
-				iframe.width 	= '560';
-				iframe.height 	= '315';
-				iframe.src 		= 'https://www.youtube.com/embed/TuapDf6Un7M';
-				// iframe.frameborder = '0';
-				// iframe.allowfullscreen = true;
-
-				// container.appendChild(iframe);
-			}
-		}
+		iframe.width 	= '560';
+		iframe.height 	= '315';
+		iframe.src 		= 'https://www.youtube.com/embed/_F6JwJ_i-Ls';
+	}
+}
 		
 
 
 function iOSversion() { 
-	if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) { 
-		// if (!!window.indexedDB) { return 'iOS 8 and up'; } 
-		// if (!!window.SpeechSynthesisUtterance) { return 'iOS 7'; } 
-		// if (!!window.webkitAudioContext) { return 'iOS 6'; } 
-		// if (!!window.matchMedia) { return 'iOS 5'; } 
-		// if (!!window.history && 'pushState' in window.history) 
-		// 	{ return 'iOS 4'; } 
-		// return 'iOS 3 or earlier';
-		return true;
-	} 
-	// return 'Not an iOS device';
-	return false;
+	return (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream);
 }
 
 document.addEventListener('DOMContentLoaded', ready);
