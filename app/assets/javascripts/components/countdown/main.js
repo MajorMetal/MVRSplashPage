391e465f2a294fb3e81bd101a470904d3c879b82
samjwalston <samjwalston@gmail.com>
2016-07-24 17:09:28 -0700

    Removed socalvrcon event

--- app/assets/javascripts/components/countdown/main.js
+++ app/assets/javascripts/components/countdown/main.js
@@ -10,14 +10,4 @@ var countdownController = function() {
     minutes.textContent = event.strftime('%M');
   });
 
-  $('#socalvr-timer').countdown('10/29/2016 12:00 PM', function(event) {
-    var days    = this.querySelector('.days');
-    var hours   = this.querySelector('.hours');
-    var minutes = this.querySelector('.minutes');
-
-    days.textContent    = event.strftime('%-D');
-    hours.textContent   = event.strftime('%H');
-    minutes.textContent = event.strftime('%M');
-  });
-
 };
