f6bf4418326336ff910774b1c72e9f9e99598fc3
samjwalston <samjwalston@gmail.com>
2016-09-09 17:44:15 -0700

    Added forward to devpost hackathon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
     address: 'smtp.sendgrid.net',
     port: '587',
     domain: 'heroku.com',
-    user_name: ENV.fetch('SENDGRID_USERNAME'),
-    password: ENV.fetch('SENDGRID_PASSWORD'),
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     authentication: :plain,
     enable_starttls_auto: true
   }
